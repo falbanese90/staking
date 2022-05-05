0a3e21c64614e4a31b7542b2abb5967a121125df
Frank Albanese <albanesefc9@gmail.com>
2022-05-04 22:29:24 -0400

    Staking minimally functional

--- src/handlers/staked.ts
+++ src/handlers/staked.ts
@@ -24,9 +24,43 @@ const add = async (req: Request, res: Response) => {
     }
 }
 
+const del = async (req: Request, res: Response) => {
+    try {
+        const pub: Staked = {
+            public_key: req.params.public_key
+        }
+        const result = await store.delete(pub);
+    } catch(err) {
+        res.send(`${err}`);
+    }
+}
+const select = async (req: Request, res: Response) => {
+    try {
+        const pub: Staked = {
+            public_key: req.params.public_key
+        }
+        const result = await store.select(pub);
+        res.send(result);
+    } catch(err) {
+        res.send(`${err}`);
+    }
+}
+const getTime = async (req: Request, res: Response) => {
+    try {
+        const pub: string = req.params.public_key;
+        const result = await store.getTime(pub);
+        res.send(result);
+    } catch(err) {
+        res.send(`${err}`);
+    }
+}
+
 const staked_store_routes = (app: express.Application) => {
     app.get('/staked', index)
     app.post('/staked/add', add);
+    app.delete('/staked/delete/:public_key', del);
+    app.get('/staked/select/:public_key', select)
+    app.get('/staked/time/:public_key', getTime);
 }
 
 export default staked_store_routes;
--- src/models/staked.ts
+++ src/models/staked.ts
@@ -12,6 +12,7 @@ export class Stake_Store {
             const conn = await Client.connect()
             const sql = 'SELECT * FROM staked';
             const result = await conn.query(sql);
+            conn.release();
             return result.rows;
         } catch (err) {
             throw new Error(`${err}`);
@@ -22,6 +23,7 @@ export class Stake_Store {
             const conn = await Client.connect();
             const sql = 'INSERT INTO staked(public_key) VALUES($1) RETURNING *';
             const result = await conn.query(sql, [pub.public_key]);
+            conn.release();
             return result.rows[0];
         } catch (err) {
             throw new Error(`${err}`);
@@ -30,8 +32,31 @@ export class Stake_Store {
     async delete(pub:Staked): Promise<void> {
         try {
             const conn = await Client.connect();
-            const sql = 'DELETE FROM staked WHERE public_key=$1';
+            const sql = 'DELETE FROM staked WHERE public_key=($1)';
             const result = await conn.query(sql, [pub.public_key]);
+            conn.release();
+        } catch(err) {
+            throw new Error(`${err}`);
+        }
+    }
+    async select(pub: Staked): Promise<Staked[]> {
+        try {
+            const conn = await Client.connect();
+            const sql = 'SELECT * FROM staked WHERE public_key=($1)';
+            const result = await conn.query(sql, [pub.public_key]);
+            conn.release();
+            return result.rows
+        } catch(err) {
+            throw new Error(`${err}`);
+        }
+    }
+    async getTime(pub: string): Promise<string> {
+        try {
+            const conn = await Client.connect();
+            const sql = 'SELECT t FROM staked WHERE public_key=($1)';
+            const result = conn.query(sql, [pub]);
+            conn.release();
+            return (await result).rows[0];
         } catch(err) {
             throw new Error(`${err}`);
         }
